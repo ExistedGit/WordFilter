e60b20c9f096373abeeeb63e068a2bfa6a758a14
DedFama <clash2clans1one@gmail.com>
2022-04-22 17:57:57 +0200

    + variables to display overall progress

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WordFilter
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public int NumberOfFilesToAnalysis = 0;
+        public int NumberOfCurrentProgress = 0;
+
+
         public MainWindow()
         {
             InitializeComponent();
