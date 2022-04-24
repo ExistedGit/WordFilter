8f8d0342e59c798d656454ed03c506c2b4f072ef
ExistedGit <paytvemail@gmail.com>
2022-04-24 17:08:48 +0300

    Рефакторинг

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,8 +7,8 @@ namespace WordFilter
 
     public partial class MainWindow : Window
     {
-        public int NumberOfFilesToAnalysis { get; set; } = 0;
-        public int NumberOfCurrentProgress { get; set; } = 0;
+        public int TotalFileCount { get; set; } = 0;
+        public int AnalyzedFileCount { get; set; } =0;
         public List<Analyzer> Analyzers { get; set; }
 
         public MainWindow()
