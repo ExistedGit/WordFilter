324dc0159b61074edd11bc8d6bb4e83db5922876
ExistedGit <paytvemail@gmail.com>
2022-04-25 12:31:23 +0300

    + Метод для инициализации списка анализаторов

---
+++ MainWindow.Drive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordFilter.Entities;
+
+namespace WordFilter
+{
+    partial class MainWindow
+    {
+        private List<Analyzer> CreateAnalyzers()
+        {
+            List<Analyzer> result = new List<Analyzer>();
+            foreach (var drive in DriveInfo.GetDrives()) {
+                if (drive.IsReady)
+                    result.Add(new Analyzer(drive.RootDirectory.FullName));
+            };
+            return result;
+        }
+    }
+}
