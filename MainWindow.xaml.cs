ec7f282d7357fdcc84c01016cf6dc32971cfde19
ExistedGit <paytvemail@gmail.com>
2022-04-25 22:14:36 +0300

    "Open..." menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using WordFilter.Entities;
 
+using WordFilter.Entities;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
 namespace WordFilter
 {
 
@@ -13,7 +17,7 @@ namespace WordFilter
         public int AnalyzedFileCount { get; set; } = 0;
 
         public ObservableCollection<Analyzer> Analyzers { get; set; }
-
+        public ObservableCollection<string> BannedStrings { get; set; }
 
         public MainWindow()
         {
@@ -31,6 +35,27 @@ namespace WordFilter
         {
             MessageBox.Show(((Button)sender).Tag.GetType().ToString());
 
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            if (item.Name.Equals("OpenMenu"))
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "WordFilter Config Files|*.wfc";
+                dialog.Title = "Открыть файл конфигурации...";
+                dialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    using(StreamReader reader = new StreamReader(dialog.FileName))
+                    {
+                        string text = reader.ReadToEnd().Trim();
+                        BannedStrings = new ObservableCollection<string>(text.Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries));
+                    }
+                }
+            }
+            
         }
     }
 }
